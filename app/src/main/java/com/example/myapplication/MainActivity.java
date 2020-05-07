ce92e0656632597310f45cc5f3bf66152229f9e2
ss <asmanaimi13@gmail.com>
2020-05-07 07:02:46 +0100

    ajout function delete task +alertdialog+yoga

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
 
         setTaskDescription( new ActivityManager.TaskDescription("Shape it up",bitmap));
